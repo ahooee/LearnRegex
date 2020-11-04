af1ca4104d403ccd12adb692713c43e45b87b4ca
ahooee <ahooyee@gmail.com>
2020-11-04 17:31:42 +0330

    اضافه کردن اکتیوتی انتخاب

--- app/src/main/java/ir/linuxian/learnregex/activities/EntekhabActivity.java
+++ app/src/main/java/ir/linuxian/learnregex/activities/EntekhabActivity.java
@@ -2,18 +2,15 @@ package ir.linuxian.learnregex.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import ir.linuxian.learnregex.R;
 
-public class SplashActivity extends AppCompatActivity {
+public class EntekhabActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        if(true)
-            startActivity(new Intent(this,EntekhabActivity.class));
+        setContentView(R.layout.activity_entekhab);
     }
 }
