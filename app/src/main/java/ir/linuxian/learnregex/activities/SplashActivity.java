94e0f453cf9586f71bc6e6b5da916100f8b29065
ahooee <ahooyee@gmail.com>
2020-11-04 17:10:22 +0330

    شروع!!!

---
+++ app/src/main/java/ir/linuxian/learnregex/activities/SplashActivity.java
@@ -0,0 +1,16 @@
+package ir.linuxian.learnregex.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ir.linuxian.learnregex.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
